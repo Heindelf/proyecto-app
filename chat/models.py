52225827e4cf9541ea407ea4348ad1f987db5fd4
Christian <naresan50@hotmail.com>
2024-11-30 00:48:23 -0500

    first commit

---
+++ chat/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    profrile_pictures = models.ImageField(upload_to="profile_pictures/",blank=True,null=True)
+
+
+class Message(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)  
